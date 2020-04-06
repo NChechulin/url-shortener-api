f5192547426be681251aa081297d3b6ccfc2661c
Nikolay Chechulin <chechulin646@gmail.com>
2020-04-06 23:25:36 +0300

    add shortening link

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import db
+import validation
+import coding
 from json import dumps
 from flask import Flask, request
 
@@ -9,7 +11,7 @@ app = Flask(__name__)
 @app.route("/api/get_url", methods=['GET'])
 def get_url():
     response = {}
-    
+
     code = request.args.get('code')
     url = db.get_url(code)
 
@@ -21,6 +23,31 @@ def get_url():
     response['success'] = True
     response['message'] = 'success'
     response['url'] = url
+
+    return dumps(response)
+
+
+@app.route("/api/shorten_link", methods=['GET'])
+def shorten_link():
+    response = {}
+
+    url = request.args.get('url')
+    code = coding.try_encode(url)
+
+    if code is None:
+        response['success'] = False
+        response['message'] = 'sent link is not a valid URL'
+        return dumps(response)
+
+    result = db.add_url(code, url)
+
+    if result is None:
+        response['success'] = False
+        response['message'] = 'could not add URL to database'
+
+    response['success'] = True
+    response['message'] = 'success'
+    response['code'] = code
 
     return dumps(response)
 
