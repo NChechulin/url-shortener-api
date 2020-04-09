b14196edc9e0655db3a6ff1e937d9b1d1819fc6a
Nikolay Chechulin <chechulin646@gmail.com>
2020-04-09 14:30:44 +0300

    make coding try to search if the url was already used

--- coding.py
+++ coding.py
@@ -1,6 +1,7 @@
 from random import choice
 import validation
 import string
+import db
 
 
 alphabet = string.digits + string.ascii_lowercase + string.ascii_uppercase
@@ -14,8 +15,8 @@ def __generate_code(size=7):
 def try_encode(user_input):
     """returns code, if user_input is a valid URL"""
     if validation.validate_url(user_input):
-        code = __generate_code()
-        url = user_input
+        url = user_input.strip()
+        code = db.try_get_code(url)
 
         # if code is occupied, generate a new one with different length
         while not validation.validate_code(code):
